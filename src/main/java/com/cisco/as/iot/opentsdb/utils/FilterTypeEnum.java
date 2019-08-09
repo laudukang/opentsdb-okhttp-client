099a6c123179f22553e91872b78306843cc39bc7
laudukang <751611201@qq.com>
2019-08-09 18:06:18 +0800

    format FilterTypeEnum doc

--- src/main/java/com/cisco/as/iot/opentsdb/utils/FilterTypeEnum.java
+++ src/main/java/com/cisco/as/iot/opentsdb/utils/FilterTypeEnum.java
@@ -6,14 +6,15 @@ package com.cisco.as.iot.opentsdb.utils;
  * <p>Date: 2019/8/7
  * <p>Time: 16:54
  * <p>Version: 1.0
+ * See http://opentsdb.net/docs/build/html/user_guide/query/filters.html#built-in-2-x-filters
  */
 public enum FilterTypeEnum {
 
-    literal_or,//精确匹配多项迭代值，多项迭代值以'|'分隔，大小写敏感
-    ilteral_or,//精确匹配多项迭代值，多项迭代值以'|'分隔，忽略大小写
-    not_literal_or,//通配符取非匹配，大小写敏感
-    not_iliteral_or,//通配符取非匹配，忽略大小写
-    wildcard,//通配符匹配，大小写敏感
-    iwildcard,//通配符匹配，忽略大小写
-    regexp//正则表达式匹配
+    literal_or,         //   精确匹配多项迭代值，多项迭代值以'|'分隔，大小写敏感
+    ilteral_or,         //   精确匹配多项迭代值，多项迭代值以'|'分隔，忽略大小写
+    not_literal_or,     //   通配符取非匹配，大小写敏感
+    not_iliteral_or,    //  通配符取非匹配，忽略大小写
+    wildcard,           //  通配符匹配，大小写敏感
+    iwildcard,          //  通配符匹配，忽略大小写
+    regexp              //  正则表达式匹配
 }
